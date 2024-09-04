caa378e27a7f2829898e9aa6f9c1100f5741618b
kezdetphia <fehermark88@gmail.com>
2024-09-04 13:04:59 -0700

    add image upload hook isntead of handling in the addproduct file

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,7 +1,4 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
-import { imageDb } from "../../firebase/firebaseConfig";
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import { v4 as uuidv4 } from "uuid";
 import {
   IonButton,
   IonImg,
@@ -20,16 +17,23 @@ import {
   IonCardContent,
 } from "@ionic/react";
 import axios from "axios";
-import heic2any from "heic2any";
+import useImageUpload from "../../hooks/useImageUpload";
 
 function AddProduct({ user, setUserInfo, setProductsUpdated }) {
-  const [selectedImages, setSelectedImages] = useState([]);
-  const [uploadedImageUrls, setUploadedImageUrls] = useState([]);
+  const {
+    selectedImages,
+    uploadedImageUrls,
+    loading,
+    error,
+    handleFileChange,
+    allImagesUploaded,
+    setUploadedImageUrls, // Destructure the state setter
+    setSelectedImages, // Destructure the state setter
+    setAllImagesUploaded, // Destructure the state setter
+  } = useImageUpload();
+
   const [formData, setFormData] = useState({ name: "", description: "" });
   const [isSubmitDisabled, setIsSubmitDisabled] = useState(true);
-  const [allImagesUploaded, setAllImagesUploaded] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null); // State to handle errors
   const fileInputRef = useRef(null); // Create ref for file input
 
   // Function to check form validity
@@ -46,39 +50,6 @@ function AddProduct({ user, setUserInfo, setProductsUpdated }) {
     validateForm();
   }, [selectedImages.length, formData, validateForm]);
 
-  const handleUpload = useCallback(() => {
-    setIsSubmitDisabled(true);
-    // setLoading(true); // Show loading indicator
-
-    if (selectedImages.length > 0) {
-      const uploadPromises = selectedImages.map((image) => {
-        const imageRef = ref(imageDb, `files/${uuidv4()}`);
-        return uploadBytes(imageRef, image).then((snapshot) =>
-          getDownloadURL(snapshot.ref)
-        );
-      });
-
-      Promise.all(uploadPromises)
-        .then((urls) => {
-          const uniqueUrls = [...new Set([...uploadedImageUrls, ...urls])];
-          setUploadedImageUrls(uniqueUrls);
-          setAllImagesUploaded(true);
-          validateForm(); // Revalidate form after upload
-          setLoading(false); // Hide loading indicator after upload completes
-        })
-        .catch((error) => {
-          console.error("Failed to upload images", error);
-          setLoading(false); // Hide loading indicator on error
-        });
-    }
-  }, [selectedImages, uploadedImageUrls, validateForm]);
-
-  useEffect(() => {
-    if (selectedImages.length > 0 && !allImagesUploaded) {
-      handleUpload();
-    }
-  }, [selectedImages, allImagesUploaded, handleUpload]);
-
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevData) => {
@@ -86,45 +57,6 @@ function AddProduct({ user, setUserInfo, setProductsUpdated }) {
       validateForm(); // Revalidate form whenever input changes
       return newFormData;
     });
-  };
-
-  const handleFileChange = async (e) => {
-    setLoading(true); // Start loader as soon as file selection begins
-
-    const files = Array.from(e.target.files);
-    const totalFiles = selectedImages.length + files.length;
-
-    if (totalFiles > 5) {
-      setError("You can only upload a maximum of 5 images.");
-      setLoading(false);
-      return;
-    }
-
-    const convertedFiles = await Promise.all(
-      files.map(async (file) => {
-        if (file.type === "image/heic") {
-          try {
-            const convertedBlob = await heic2any({
-              blob: file,
-              toType: "image/jpeg",
-            });
-            return new File([convertedBlob], `${file.name}.jpeg`, {
-              type: "image/jpeg",
-            });
-          } catch (error) {
-            console.error("Failed to convert HEIC to JPEG", error);
-            return null;
-          }
-        }
-        return file;
-      })
-    );
-
-    const validFiles = convertedFiles.filter(Boolean);
-    setSelectedImages((prevImages) => [...prevImages, ...validFiles]);
-    setAllImagesUploaded(false); // Reset flag until images are uploaded
-
-    setLoading(false); // Hide loader after file processing is done
   };
 
   const handleSubmit = async () => {
