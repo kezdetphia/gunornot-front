01fa7d1b13c142245d99b968e1e8b7939879470f
kezdetphia <fehermark88@gmail.com>
2024-09-04 13:40:20 -0700

    fix unused app import in index.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
+import React from "react";
 import { Outlet } from "react-router-dom";
+// Theme variables
+import "./theme/variables.css";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "./context/authContext";
 
-/* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
 import {
   IonButton,
   IonButtons,
@@ -23,14 +17,6 @@ import {
   IonToolbar,
   setupIonicReact,
 } from "@ionic/react";
-import React from "react";
-import { useNavigate } from "react-router-dom";
-
-// Theme variables
-import "./theme/variables.css";
-import { useAuth } from "./context/authContext";
-
-setupIonicReact();
 
 function App() {
   const { signOut } = useAuth();
--- src/index.js
+++ src/index.js
@@ -1,21 +1,39 @@
+/* Core CSS required for Ionic components to work properly */
+import "@ionic/react/css/core.css";
+import "@ionic/react/css/normalize.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/typography.css";
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/float-elements.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/display.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import App from "./App";
+import { setupIonicReact } from "@ionic/react";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import NotFound from "./pages/NotFound";
-import ProtectedRoute from "./routes/ProtectedRoute";
-import PublicRoute from "./routes/PublicRoute";
+import ProtectedRoute from "./utils/ProtectedRoute";
+import PublicRoute from "./utils/PublicRoute";
 import Layout from "./components/Layout";
 import { AuthContextProvider, useAuth } from "./context/authContext";
 import "./index.css";
+// Theme variables
+import "./theme/variables.css";
+
+// Initialize Ionic React
+setupIonicReact();
 
 const Main = () => {
   const { isAuthenticated } = useAuth();
+
   return (
     <BrowserRouter>
       <Routes>
